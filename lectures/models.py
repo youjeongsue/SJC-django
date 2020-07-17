93e7c44ac551a716d2e70bbbf41d255bf48f0e69
youjeongsue <43227174+youjeongsue@users.noreply.github.com>
2020-07-17 20:41:30 +0900

    Add video field to assignment and implement serializer for assignment, comment

--- lectures/models.py
+++ lectures/models.py
@@ -2,22 +2,32 @@ from django.db import models
 
 class Lecture(models.Model):
     lecturename = models.CharField(max_length=100)
-    professor = models.ForeignKey('accounts.User', related_name="professor_id", on_delete=models.CASCADE, null=True, blank=True)
-    students = models.ManyToManyField('accounts.User', related_name="student_id", null=True, blank=True)
+    professor = models.ForeignKey('accounts.User', related_name="professor_id", on_delete=models.CASCADE, blank=True)
+    students = models.ManyToManyField('accounts.User', related_name="student_id", blank=True)
 
     def __str__(self):
         return self.lecturename
 
 class Assignment(models.Model):
     assignmentname = models.CharField(max_length=100)
-    lecture = models.ForeignKey(Lecture, related_name="assignments", on_delete=models.CASCADE, null=True)
+    lecture = models.ForeignKey(Lecture, related_name="assignments", on_delete=models.CASCADE)
+    professor_video = models.FileField(blank=True)
+    student_video = models.ManyToManyField('Student_video', related_name="student_videos", on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return self.assignmentname
 
+class Student_video(models.Model):
+    student = models.ForeignKey('accounts.User', related_name="videos", on_delete=models.CASCADE)
+    student_video = models.FileField(blank=True)
+
+    def __str__(self):
+        return self.student_video.name
+
 class Comment(models.Model):
-    assignment = models.ForeignKey(Assignment, related_name="comments", on_delete=models.CASCADE, null=True)
-    content = models.CharField(max_length=300)
+    assignment = models.ForeignKey(Assignment, related_name="comments", on_delete=models.CASCADE)
+    contents = models.CharField(max_length=300)
+    timestamp = models.TimeField()
 
     def __str__(self):
         return self.assignment
--- lectures/serializers.py
+++ lectures/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Lecture
+from .models import Lecture, Assignment, Student_video, Comment
 
 class LectureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lecture
         fields = ['id','lecturename','professor','students']
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = ['id', 'assignmentname', 'lecture', 'professor_video', 'student_video']
+
+class StudentVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student_video
+        fields = ['id', 'student', 'student_video']
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'assignment', 'contents', 'timestamp']
